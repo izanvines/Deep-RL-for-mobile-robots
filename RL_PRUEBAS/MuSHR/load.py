a8ae29f29e0056487182a459a672216b28a4fe85
IzanV <izanvines@gmail.com>
2025-04-15 19:18:25 +0200

    Se ha actualizado el cargar el modelo en consocnancia con el entrenamiento, haciendo el paso a gymnasium

--- RL_PRUEBAS/MuSHR/load.py
+++ RL_PRUEBAS/MuSHR/load.py
@@ -1,14 +1,32 @@
-import gym
+import gymnasium
 from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import VecNormalize
+import numpy as np
+import torch
 
-# Crear el entorno y envolverlo en un DummyVecEnv
-env = gym.make("rover", render_mode="human")
-env = DummyVecEnv([lambda: env])  # Envolver el entorno en un VecEnv
+# --- SEMILLA ---
+SEED = 42  # o el número que prefieras
+np.random.seed(SEED)
+torch.manual_seed(SEED)
 
-# Cargar el modelo
-models_dir = "models_Reacher_140125_1/PPO"
-model_path = f"{models_dir}/200000"
+# === CONFIGURACIÓN ===
+models_dir = "models_AMR_150425_2/PPO"
+model_path = f"{models_dir}/3505000"  # Usa el modelo que quieras
+normalize_path = "models_AMR_150425_2/vec_normalize.pkl"
+
+# === CREAR Y ENVOLVER ENTORNO ===
+def make_env():
+    #return gym.make("MuSHREnv-v0")
+    #return gym.make("MuSHREnv-v0", render_mode="human") #-> con entorno gráfico
+    #return gymnasium.make("MuSHREnv-v0")
+    return gymnasium.make("MuSHREnv-v0", render_mode="human") #-> con entorno gráfico
+
+env = make_vec_env(make_env, n_envs=1, seed=SEED) #Por defeecto usa gymnasium -> ya se ha añadido el entorno a gymnasium para poder usarlo (menos warnings)
+
+# IMPORTANTE: desactivar el update de stats para reproducir correctamente
+env.training = False
+env.norm_reward = False
 
 model = PPO.load(model_path, env=env)
 
@@ -18,7 +36,6 @@ for ep in range(episodes):
     # Resetear el entorno y obtener la observación
     obs = env.reset()
     done = False
-
     while not done:
         env.render()
         action, _ = model.predict(obs)  # El modelo ahora toma la observación (obs)
